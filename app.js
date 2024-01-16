c31d6ba0a33541a291757146a889445f987522f3
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2024-01-16 14:36:41 +0700

    feat(server) : Create API from server side

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import express from "express";
+import cors from "cors";
+import { client } from "./utils/db.js";
+import questionRouter from "./apps/questionRouter.js";
+import bodyParser from "body-parser";
 
 async function init() {
   const app = express();
   const port = 4000;
 
+  await client.connect();
+
+  app.use(cors());
+
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
-  app.get("/", (req, res) => {
-    return res.json("Hello Skill Checkpoint #2");
-  });
-
-  app.get("*", (req, res) => {
-    return res.status(404).json("Not found");
-  });
+  app.use("/questions", questionRouter);
 
   app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
